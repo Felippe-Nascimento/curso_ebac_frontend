900b124bef5c6fdcb164924601e3d1b8879d4baa
Felippe-Nascimento <felippenascimento18@gmail.com>
2024-08-19 14:43:17 -0300

    Finalizando validacao

---
+++ main.js
@@ -0,0 +1,23 @@
+let form = document.getElementById('formulario');
+const numeroCampoA = document.getElementById('numeroA');
+const numeroCampoB = document.getElementById('numeroB');
+
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    formValido();
+    limpaTela();
+});
+
+function formValido (){
+    if (numeroCampoA >= numeroCampoB){
+        alert('Formulário INVÁLIDO, Campo B não é maior que o Campo A.')
+    } else {
+        alert('Formulário VÁLIDO, Campo B é maior que o Campo A.');
+    }
+}
+
+function limpaTela() {
+    numeroCampoA.value = '';
+    numeroCampoB.value = '';
+}
