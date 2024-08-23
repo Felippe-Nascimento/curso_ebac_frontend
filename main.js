d07953c93423d7bb708bc5293ffaa773dc0b5324
Felippe-Nascimento <felippenascimento18@gmail.com>
2024-08-23 13:58:31 -0300

    Atualização da validção

--- main.js
+++ main.js
@@ -11,13 +11,27 @@ form.addEventListener('submit', function(event){
 
 function formValido (){
     if (numeroCampoA.value >= numeroCampoB.value){
-        alert('Formulário INVÁLIDO, Campo B não é maior que o Campo A.')
+        menssagemErro();
     } else {
-        alert('Formulário VÁLIDO, Campo B é maior que o Campo A.');
+        menssagemSucesso();
     }
 }
 
 function limpaTela() {
     numeroCampoA.value = '';
     numeroCampoB.value = '';
+}
+
+function menssagemErro(){
+    let menssagemErroForm = document.querySelector('.menssagem');
+    menssagemErroForm.innerHTML = '<b>Formulário Inválido:</b> Campo B não é maior que o Campo A.';
+    menssagemErroForm.style.display = 'block';
+    menssagemErroForm.style.backgroundColor = 'red';
+}
+
+function menssagemSucesso() {
+    let menssagemSucessoForm = document.querySelector('.menssagem');
+    menssagemSucessoForm.innerHTML = '<b>Formulário VÁLIDO:</b> Campo B é maior que o Campo A';
+    menssagemSucessoForm.style.display = 'block';
+    menssagemSucessoForm.style.backgroundColor = 'green';
 }
